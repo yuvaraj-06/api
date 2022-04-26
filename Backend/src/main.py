e41e3e61324a3be528bfc2b429052b83b59478a0
Yuvaraj Tankala <59510075+yuvaraj-06@users.noreply.github.com>
2022-04-26 10:19:36 -0700

    Update main.py

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,2 +1,17 @@
-def app(event):
-    return "Hello, world!"
+from fastapi import FastAPI, Request
+from model import mine_data
+import uvicorn
+app = FastAPI()
+
+ 
+
+@app.get("/")
+def test(request: Request):
+    return {"testing passed"}
+@app.post("/grade")
+def grade():
+        res=mine_data("yuvarajcoder@gmail.com","yuvaraj1234")
+        print(res)
+        return {"output":res}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
